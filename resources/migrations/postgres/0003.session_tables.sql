e45c292af1f65e2f858512901be8ff88703b2311
willtanium <williamkibira@gmail.com>
2021-09-06 00:00:59 +0300

    - Added sql migrations to create the session tables

---
+++ resources/migrations/postgres/0003.session_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE session_tb (
+    id BIGSERIAL PRIMARY KEY,
+    device_identifier VARCHAR NOT NULL,
+	user_id BIGINT NOT NULL,
+	initiated_at  TIMESTAMP NOT NULL,
+	expires_at  TIMESTAMP NOT NULL,
+	status VARCHAR NO NULL DEFAULT 'ACTIVE',
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	idx BIGSERIAL,
+	CONSTRAINT user_fkey FOREIGN KEY (user_id) REFERENCES user_tb(id) ON DELETE CASCADE
+);
